9ac332bcb73fc29a0517cfb96dea05e517b4980d
Veronika Sirak <7nicka@gmail.com>
2021-04-12 15:27:03 +0100

    add close-modal function

--- js/gallery.js
+++ js/gallery.js
@@ -2,9 +2,10 @@ import images from './gallery-items.js';
 
 const listGalleryEl = document.querySelector('.js-gallery');
 const galleryImagesEl = document.querySelectorAll('.gallery__image');
+const modalContainerEl = document.querySelector('.js-lightbox');
 const lightboxImageEl = document.querySelector('.lightbox__image');
+const lightboxOverlayEl = document.querySelector('.lightbox__overlay');
 
-// Create Markup for items and insert into the gallery list
 // Создание и рендер разметки по массиву данных и предоставленному шаблону.
 
 const makeGalleryItemMarkup = ({ preview, original, description }) => {
@@ -28,20 +29,21 @@ const galleryItems = images.map(makeGalleryItemMarkup).join('');
 listGalleryEl.insertAdjacentHTML('beforeend', galleryItems);
 
 //Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
-const modalContainerEl = document.querySelector('.js-lightbox');
 listGalleryEl.addEventListener('click', onItemGalleryClick);
 
 function onItemGalleryClick(event) {
-  if (event.target.nodeName !== 'IMG') {
+  const galleryImg = event.target;
+  if (galleryImg.nodeName !== 'IMG') {
     return;
   }
   event.preventDefault();
-  openModalWindow();
-  addAtribute(event.target.dataset.source);
+  openModal();
+  addAtribute(galleryImg.dataset.source);
 }
 
 // Открытие модального окна по клику на элементе галереи.
-function openModalWindow() {
+function openModal() {
+  window.addEventListener('keydown', onEscKeyPress);
   modalContainerEl.classList.add('is-open');
 }
 
@@ -51,17 +53,36 @@ function addAtribute(url) {
 }
 
 //Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
-modalContainerEl.addEventListener('click', onModalContainerClick);
-
-function onModalContainerClick(event) {
+modalContainerEl.addEventListener('click', onModalButtonClick);
+function onModalButtonClick(event) {
   const closeBtnEl = document.querySelector('[data-action="close-lightbox"]');
   if (closeBtnEl !== event.target) {
     return;
   }
+  closeModal();
+}
+
+// Закрытие модального окна по клику на div.lightbox__overlay.
+modalContainerEl.addEventListener('click', onModalBackdropClick);
+function onModalBackdropClick(event) {
+  if (lightboxOverlayEl !== event.target) {
+    return;
+  }
+  closeModal();
+}
+// Закрытие модального окна по нажатию клавиши ESC.
+function onEscKeyPress(event) {
+  if (event.code === 'Escape') {
+    closeModal();
+  }
+}
+
+function closeModal() {
+  window.removeEventListener('keydown', onEscKeyPress);
   modalContainerEl.classList.remove('is-open');
   removeAtribute(lightboxImageEl);
 }
 //Очистка значения атрибута src элемента img.lightbox__image.
-function removeAtribute(event) {
-  event.src = '';
+function removeAtribute(image) {
+  image.src = '';
 }
